18e80128f4f0864d0759de7eede605859abb70ee
joeschine <joseph.schine@gmail.com>
2021-11-07 13:04:09 -0500

    Update joetestfulltest.py
    testing farPlane of 600

--- joetestfulltest.py
+++ joetestfulltest.py
@@ -25,7 +25,7 @@ minFallTime = 0.25 # time (s) of fastest feather cycle
 maxFallTime = 2 # time (s) of slowest feather cycle
 
 nearPlane = 300  # distance at nearest point before errors
-farPlane = 800 # distance at background
+farPlane = 600 # distance at background
 
 movementTrigger = 5000 # amount of movement needed to trigger an expression
 
